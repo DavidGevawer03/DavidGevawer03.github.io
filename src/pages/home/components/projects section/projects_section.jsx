71eb5c1484b63866efba24a34b4cf8d296c2f060
David Gevawer <davidlgevawer@gmail.com>
2024-08-28 11:36:31 -0600

    Changing absolute paths to relative paths

--- src/pages/home/components/projects section/projects_section.jsx
+++ src/pages/home/components/projects section/projects_section.jsx
@@ -6,6 +6,9 @@ import FlutterLogo from "/src/assets/icons/flutter.svg";
 import ReactLogo from "/src/assets/icons/react.svg";
 import NestJsLogo from "/src/assets/icons/nestjs.svg";
 import PostgreSQLLogo from "/src/assets/icons/postgresql.svg";
+import CampusConnectImage from "/src/assets/projects/campusconnect.webp";
+import CrewStockImage from "/src/assets/projects/crewstock.webp";
+import MLGHubImage from "/src/assets/projects/mlghub.webp";
 
 const ProjectsSection = () => {
   return (
@@ -14,7 +17,7 @@ const ProjectsSection = () => {
       <div className={Styles.divWrap}>
         <Link to="/project/campusconnect" className={Styles.projectLink}>
           <ProjectCard
-            image="/src/assets/projects/campusconnect.webp"
+            image={CampusConnectImage}
             title="Campus Connect: UNAH-VS"
             description="Campus Connect: UNAH-VS es una red social estudiantil con un toque especial. Ofrece un marketplace, noticias, entretenimiento y personalización de perfiles, todo en un solo lugar."
             icon1={<img src={FlutterLogo} className={Styles.imageIcons} />}
@@ -23,7 +26,7 @@ const ProjectsSection = () => {
         </Link>
         <Link to="/" className={Styles.projectLink}>
           <ProjectCard
-            image="/src/assets/projects/crewstock.webp"
+            image={CrewStockImage}
             title="Crew Stock"
             description="Crew Stock es tu destino en línea definitivo donde comprar tus zapatos, ropa y accesorios. Con un diseño elegante y fácil de usar, Crew Stock ofrece una experiencia de compra inigualable, permitiéndote explorar una amplia variedad de productos cuidadosamente seleccionados para adaptarse a todos los estilos y ocasiones."
             icon1={<img src={FlutterLogo} className={Styles.imageIcons} />}
@@ -47,12 +50,16 @@ const ProjectsSection = () => {
 
         <Link to="/" className={Styles.projectLink}>
           <ProjectCard
-            image="/src/assets/projects/mlghub.webp"
+            image={MLGHubImage}
             title="MLG Hub"
             description="MLG Hub es una plataforma innovadora diseñada específicamente para Multiservicio Lara Gevawer, proporcionando una solución integral para la gestión de recursos y operaciones empresariales. Este proyecto consta de una landing page pública y una aplicación interna robusta con múltiples dashboards y aplicaciones internas, facilitando una gestión eficiente y centralizada."
             icon1={<img src={FlutterLogo} className={Styles.imageIcons} />}
             icon2={<img src={ReactLogo} className={Styles.imageIconsAlt} />}
             icon3={<img src={FirebaseLogo} className={Styles.imageIconsAlt} />}
+            icon4={<img src={NestJsLogo} className={Styles.imageIconsAlt} />}
+            icon5={
+              <img src={PostgreSQLLogo} className={Styles.imageIconsAlt} />
+            }
           />
         </Link>
 
